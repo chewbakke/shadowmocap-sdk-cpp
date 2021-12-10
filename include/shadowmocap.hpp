966231257a8e92238129580d2770ea8fe114ba49
Luke Tokheim <luketokheim@gmail.com>
2021-12-09 19:12:35 -0800

    Prototyping. Initial check in

---
+++ include/shadowmocap.hpp
@@ -0,0 +1,157 @@
+#if !defined(SHADOWMOCAP_USE_BOOST)
+#define SHADOWMOCAP_USE_BOOST 1
+#endif
+
+#if SHADOWMOCAP_USE_BOOST
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/udp.hpp>
+#include <boost/asio/read.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/asio/write.hpp>
+#else
+#include <asio/awaitable.hpp>
+#include <asio/connect.hpp>
+#include <asio/detached.hpp>
+#include <asio/ip/tcp.hpp>
+#include <asio/read.hpp>
+#include <asio/use_awaitable.hpp>
+#include <asio/write.hpp>
+#endif
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace shadowmocap {
+
+#if SHADOWMOCAP_USE_BOOST
+namespace net = boost::asio;
+#else
+namespace net = asio;
+#endif
+
+using net::ip::tcp;
+using net::ip::udp;
+
+template <typename Socket>
+class datastream {
+public:
+  Socket socket;
+  std::vector<std::string> name_map;
+}; // class datastream
+
+template <typename Message>
+bool is_metadata(const Message &message)
+{
+  const std::string XmlMagic = "<?xml";
+
+  if (std::size(message) < std::size(XmlMagic)) {
+    return false;
+  }
+
+  if (!std::equal(
+        std::begin(XmlMagic), std::end(XmlMagic), std::begin(message))) {
+    return false;
+  }
+
+  return true;
+}
+
+template <typename Message, typename AsyncReadStream>
+net::awaitable<Message> read_message(AsyncReadStream &s)
+{
+  static_assert(
+    sizeof(typename Message::value_type) == sizeof(char),
+    "message is not bytes");
+
+  unsigned length = 0;
+  co_await net::async_read(
+    s, net::buffer(&length, sizeof(length)), net::use_awaitable);
+
+  length = ntohl(length);
+  Message message(length, 0);
+
+  co_await net::async_read(s, net::buffer(message), net::use_awaitable);
+
+  co_return message;
+}
+
+template <typename Message, typename Socket>
+net::awaitable<Message> read_message(datastream<Socket> &stream)
+{
+  auto message = co_await read_message<Message>(stream.socket);
+
+  if (is_metadata(message)) {
+    std::cout << std::string(message.begin(), message.end()) << "\n";
+
+    message = co_await read_message<Message>(stream.socket);
+  }
+
+  co_return message;
+}
+
+template <typename Message, typename AsyncWriteStream>
+net::awaitable<void> write_message(AsyncWriteStream &s, const Message &message)
+{
+  static_assert(
+    sizeof(typename Message::value_type) == sizeof(char),
+    "message is not bytes");
+
+  unsigned length = htonl(static_cast<unsigned>(std::size(message)));
+  co_await net::async_write(
+    socket, net::buffer(&length, sizeof(length)), net::use_awaitable);
+
+  co_await net::async_write(
+    socket, net::const_buffer(message), net::use_awaitable);
+}
+
+template <typename Message, typename Socket>
+net::awaitable<void>
+write_message(datastream<Socket> &stream, const Message &message)
+{
+  co_await write_message<Message, Socket>(stream.socket, message);
+}
+
+template <typename EndpointSequence>
+net::awaitable<void>
+connect_to(tcp::socket &socket, const EndpointSequence &endpoints)
+{
+  // tcp::socket socket(co_await net::this_coro::executor);
+  co_await net::async_connect(socket, endpoints, net::use_awaitable);
+
+  socket.set_option(tcp::no_delay(true));
+}
+
+/*
+net::awaitable<udp::socket> connect_to(const auto &endpoints)
+{
+  co_return udp::socket(co_await net::this_coro::executor);
+}
+*/
+
+template <typename Socket, typename EndpointSequence>
+net::awaitable<datastream<Socket>>
+open_connection(const EndpointSequence &endpoints)
+{
+  Socket socket(co_await net::this_coro::executor);
+#if 0
+  co_await net::async_connect(socket, endpoints, net::use_awaitable);
+
+  socket.set_option(tcp::no_delay(true));
+#else
+  // co_await net::async_connect_to(socket, endpoints, net::use_awaitable);
+#endif
+
+  auto message = co_await read_message<std::vector<char>>(socket);
+
+  if (is_metadata(message)) {
+    std::cout << std::string(message.begin(), message.end()) << "\n";
+  }
+
+  co_return socket;
+}
+
+} // namespace shadowmocap
---
+++ test/test.cpp
@@ -0,0 +1,158 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <shadowmocap.hpp>
+
+#if SHADOWMOCAP_USE_BOOST
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/udp.hpp>
+#else
+#include <asio/co_spawn.hpp>
+#include <asio/io_context.hpp>
+#endif
+
+#include <array>
+
+/*
+#include <boost/asio.hpp>
+
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/write.hpp>
+
+
+namespace net = boost::asio;
+using boost::asio::ip::tcp;
+
+using boost::asio::awaitable;
+using boost::asio::co_spawn;
+using boost::asio::detached;
+using boost::asio::use_awaitable;
+
+namespace shadowmocap {
+
+class datastream {
+public:
+  static bool is_metadata(const std::vector<char> &message);
+
+  tcp::socket socket;
+}; // class datastream
+
+bool datastream::is_metadata(const std::vector<char> &message)
+{
+  const std::string XmlMagic = "<?xml";
+
+  if (std::size(message) < std::size(XmlMagic)) {
+    return false;
+  }
+
+  if (!std::equal(std::begin(XmlMagic), std::end(XmlMagic), message.begin())) {
+    return false;
+  }
+
+  return true;
+}
+
+awaitable<std::vector<char>> read_message(tcp::socket &socket)
+{
+  unsigned length = 0;
+  co_await net::async_read(
+    socket, net::buffer(&length, sizeof(length)), use_awaitable);
+
+  length = ntohl(length);
+  std::vector<char> message(length);
+
+  co_await net::async_read(socket, net::buffer(message), use_awaitable);
+
+  co_return message;
+}
+
+awaitable<std::vector<char>> read_message(datastream &stream)
+{
+  auto message = co_await read_message(stream.socket);
+
+  if (datastream::is_metadata(message)) {
+    std::cout << std::string(message.begin(), message.end()) << "\n";
+
+    message = co_await read_message(stream.socket);
+  }
+
+  co_return message;
+}
+
+awaitable<void> write_message(tcp::socket &socket, std::vector<char> message)
+{
+  unsigned length = htonl(static_cast<unsigned>(std::size(message)));
+  co_await net::async_write(
+    socket, net::buffer(&length, sizeof(length)), use_awaitable);
+
+  co_await net::async_write(socket, net::buffer(message), use_awaitable);
+}
+
+awaitable<void> write_message(datastream &stream, std::vector<char> message)
+{
+  co_await write_message(stream.socket, std::move(message));
+}
+
+awaitable<datastream> open_connection(tcp::endpoint endpoint)
+{
+  tcp::socket socket(co_await net::this_coro::executor);
+
+  co_await socket.async_connect(endpoint, use_awaitable);
+
+  socket.set_option(tcp::no_delay(true));
+
+  auto message = co_await read_message(socket);
+
+  if (datastream::is_metadata(message)) {
+    std::cout << std::string(message.begin(), message.end()) << "\n";
+  }
+
+  co_return socket;
+}
+
+} // namespace shadowmocap
+*/
+
+namespace net = shadowmocap::net;
+
+template <typename EndpointSequence>
+net::awaitable<void>
+read_shadowmocap_datastream(const EndpointSequence &endpoints)
+{
+  using namespace shadowmocap;
+
+  auto stream = co_await open_connection<net::ip::udp::socket>(endpoints);
+  for (;;) {
+    auto message = co_await read_message<std::string>(stream);
+  }
+}
+
+bool my_main()
+{
+  try {
+    net::io_context ctx;
+
+    auto endpoints = net::ip::tcp::resolver(ctx).resolve("127.0.0.1", "32076");
+
+    co_spawn(ctx, read_shadowmocap_datastream(endpoints), net::detached);
+
+    ctx.run();
+
+    return true;
+  } catch (std::exception &) {
+  }
+
+  return false;
+}
+
+TEST_CASE(
+  "Client can stream from Configurable service and parse messages",
+  "[Client][Format]")
+{
+  REQUIRE(my_main());
+}
