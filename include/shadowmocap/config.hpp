4637a07add895963eda44fc3cf375b411ff31a81
Luke Tokheim <luketokheim@gmail.com>
2021-12-22 11:22:33 -0800

    Added version number

--- include/shadowmocap/config.hpp
+++ include/shadowmocap/config.hpp
@@ -13,3 +13,9 @@ static_assert(BOOST_ASIO_VERSION >= 102100);
 
 static_assert(ASIO_VERSION >= 102100);
 #endif
+
+/// Integer version in MAJOR.MINOR.PATCH format with digits X.YYY.ZZ.
+/*
+ * Adheres to semantic versioning specification as per https://semver.org/
+ */
+#define SHADOWMOCAP_VERSION 400100 // 4.1.0
